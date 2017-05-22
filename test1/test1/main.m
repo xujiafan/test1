1112fccece1b8d91cefae2327d06bc7558dbfbce
许加帆 <xujiafan@xujiafandeMacBook-Pro.local>
2017-05-22 17:03:50 +0800

    my test github

---
+++ test1/test1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test1
+//
+//  Created by 许加帆 on 2017/5/22.
+//  Copyright © 2017年 许加帆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
